45fa3281cd618c0f806d2d22f87eff8715b7c0a5
tsabz <tonia.saba@gmail.com>
2020-09-20 10:44:52 -0400

    building hashtables

--- day_1.py
+++ day_1.py
@@ -13,5 +13,12 @@ arr3[arr1[1]] = arr2[1] # {1: 2, 2: 3}
 print(arr3)
 
         
+user_1 = {"name" : "Tonia Saba"}
+user_2 = {"name" : "James Stark"}
 
 
+my_user = {}
+
+my_user[user_1["name"]] = user_2["name"] #{'Tonia Saba': 'James Stark'}
+
+print(my_user)
