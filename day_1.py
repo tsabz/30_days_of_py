3de4a90384f9ac63a0d08cb2cd967fb65c467450
tsabz <tonia.saba@gmail.com>
2020-09-20 10:31:57 -0400

    lists && dictionaries

--- day_1.py
+++ day_1.py
@@ -1,1 +1,17 @@
 #hello
+
+## lists and dictionaries 
+
+arr1 = [1,2,3]
+arr2 = [2,3,4]
+
+arr3 = {}
+
+arr3[arr1[0]] = arr2[0] # {1: 2}
+arr3[arr1[1]] = arr2[1] # {1: 2, 2: 3}
+
+print(arr3)
+
+        
+
+
