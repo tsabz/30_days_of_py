d1b38b985289e42ef49eb6a6c060e77e63017ca4
tsabz <tonia.saba@gmail.com>
2020-09-21 11:56:13 -0400

    user id found with forloop

--- day_1.py
+++ day_1.py
@@ -22,3 +22,28 @@ my_user = {}
 my_user[user_1["name"]] = user_2["name"] #{'Tonia Saba': 'James Stark'}
 
 print(my_user)
+
+my_user_users = [user_1, user_2]
+
+for user in my_user_users:
+    print(user['name'])
+
+
+user_3 = {"id": 2, "name": "xy28*", "email": "abc@gmail.com"}
+user_4 = {"id": 1, "name": "Nick Person"}
+
+my_users = [user_3, user_4]
+
+for user in my_users:
+    if "email" in user:
+        print(user["email"])
+
+selected_user = {} 
+my_user_lookup = 2
+
+for user in my_users:
+    if user["id"] == my_user_lookup:
+        selected_user = user
+
+print(selected_user)
+
