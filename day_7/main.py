b6fe8a63e247aa85d066349322ac6a3ff20bf690
tsabz <tonia.saba@gmail.com>
2020-09-30 11:05:32 -0400

    send.py

---
+++ day_7/main.py
@@ -0,0 +1,22 @@
+# def my_print(txt):
+#     print(txt)
+
+
+def base_function(*args, **kwargs):
+    print(args, kwargs)
+
+
+
+
+def hello_world(txt):
+    print(txt)
+
+
+msg_template = """ Hell0 {name}, 
+Thank you for joining {website}.  We are very
+happy to have you with us. 
+""" # . format(name="Tonia", website= 'tonia.saba.com')
+
+def format_msg(my_name="Justin", my_website="toniasaba.com"):
+    my_msg = msg_template.format(name=my_name, website=my_website)
+    return my_msg
