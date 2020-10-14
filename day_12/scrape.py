72e5f545023de99999bdce18352cd974dcd48441
tsabz <tonia.saba@gmail.com>
2020-10-14 15:12:30 -0400

    element found

--- day_12/scrape.py
+++ day_12/scrape.py
@@ -1,19 +1,29 @@
 import requests
 import datetime
+from requests_html import HTML
 
 now = datetime.datetime.now()
 year =now.year
 
 
-def url_to_file(url, filename="world.html"):
+def url_to_txt(url, filename="world.html",save=False):
     r = requests.get(url)
     if r.status_code == 200:
         html_text = r.text
-        with open(f"world-{year}.html", 'w') as f:
-            f.write(html_text)
+        if save:
+            with open(f"world-{year}.html", 'w') as f:
+                f.write(html_text)
         return html_text
     return ""
 
 url = "https://www.boxofficemojo.com/year/"
 
-url_to_file(url)
+html_text = url_to_txt(url)
+
+r_html = HTML(html=html_text)
+
+table_class = ".imdb-scroll-table"
+
+r_table = r_html.find(table_class)
+
+print(r_table)
