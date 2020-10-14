394d69ca037085f007cc788152938191a03b4193
tsabz <tonia.saba@gmail.com>
2020-10-14 14:49:19 -0400

    added year to html file

--- day_12/scrape.py
+++ day_12/scrape.py
@@ -1,11 +1,15 @@
 import requests
+import datetime
+
+now = datetime.datetime.now()
+year =now.year
 
 
 def url_to_file(url, filename="world.html"):
     r = requests.get(url)
     if r.status_code == 200:
         html_text = r.text
-        with open(filename, 'w') as f:
+        with open(f"world-{year}.html", 'w') as f:
             f.write(html_text)
         return html_text
     return ""
